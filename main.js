eea138f9d6453b85f5d3e67475da28a4331b8287
Pawel <epawelkoziol@gmail.com>
2017-04-12 10:57:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function()
+{
+    document.querySelector(".page-header").classList.toggle("nav-opened");
+},false);
