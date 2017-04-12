faf9e092f305de900d743272d99400bc27ddfd9f
Pawel <epawelkoziol@gmail.com>
2017-04-12 11:00:06 +0200

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function()
-{
-    document.querySelector(".page-header").classList.toggle("nav-opened");
-},false);
