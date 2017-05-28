163099158ba53d48bd09b8c7e2aa13919e24802b
Pawel <epawelkoziol@gmail.com>
2017-05-28 21:58:43 +0200

    Add files via upload

--- zaloguj.php
+++ zaloguj.php
@@ -22,30 +22,36 @@
 		$haslo = $_POST['haslo'];
 		
 		$login = htmlentities($login, ENT_QUOTES, "UTF-8");
-		$haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
 	
 		if ($rezultat = @$polaczenie->query(
-		sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
-		mysqli_real_escape_string($polaczenie,$login),
-		mysqli_real_escape_string($polaczenie,$haslo))))
+		sprintf("SELECT * FROM uzytkownicy WHERE user='%s'",
+		mysqli_real_escape_string($polaczenie,$login))))
 		{
 			$ilu_userow = $rezultat->num_rows;
 			if($ilu_userow>0)
 			{
-				$_SESSION['zalogowany'] = true;
-				
 				$wiersz = $rezultat->fetch_assoc();
-				$_SESSION['id'] = $wiersz['id'];
-				$_SESSION['user'] = $wiersz['user'];
-				$_SESSION['drewno'] = $wiersz['drewno'];
-				$_SESSION['kamien'] = $wiersz['kamien'];
-				$_SESSION['zboze'] = $wiersz['zboze'];
-				$_SESSION['email'] = $wiersz['email'];
-				$_SESSION['dnipremium'] = $wiersz['dnipremium'];
 				
-				unset($_SESSION['blad']);
-				$rezultat->free_result();
-				header('Location: gra.php');
+				if (password_verify($haslo, $wiersz['pass']))
+				{
+					$_SESSION['zalogowany'] = true;
+					$_SESSION['id'] = $wiersz['id'];
+					$_SESSION['user'] = $wiersz['user'];
+					$_SESSION['drewno'] = $wiersz['drewno'];
+					$_SESSION['kamien'] = $wiersz['kamien'];
+					$_SESSION['zboze'] = $wiersz['zboze'];
+					$_SESSION['email'] = $wiersz['email'];
+					$_SESSION['dnipremium'] = $wiersz['dnipremium'];
+					
+					unset($_SESSION['blad']);
+					$rezultat->free_result();
+					header('Location: gra.php');
+				}
+				else 
+				{
+					$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+					header('Location: index.php');
+				}
 				
 			} else {
 				
